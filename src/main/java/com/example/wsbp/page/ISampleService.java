81f315cb13c9e6ea61be9afb7d2dce50273745ba
Posedooon <b2200930@photon.chitose.ac.jp>
2022-11-20 20:38:31 +0900

    ユーザ追加フォーム01が終了

--- src/main/java/com/example/wsbp/page/ISampleService.java
+++ src/main/java/com/example/wsbp/page/ISampleService.java
@@ -1,6 +1,12 @@
 package com.example.wsbp.page;
 
+/**
+ * 0~9までのランダムな整数を返すメソッドです。
+ * 戻り値はintです。
+ * 引数はありません。
+ * メソッド名はmakeRandIntです。
+ */
 public interface ISampleService {
-    public int makeRandInt();
+    int makeRandInt();
 
 }
--- src/main/java/com/example/wsbp/page/UserMarkerPage.java
+++ src/main/java/com/example/wsbp/page/UserMarkerPage.java
@@ -2,7 +2,9 @@ package com.example.wsbp.page;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
 import org.wicketstuff.annotation.mount.MountPath;
 
 @MountPath("UserMarker")
@@ -10,5 +12,14 @@ public class UserMarkerPage extends WebPage{
     public UserMarkerPage(){
         var toUserMakerLink = new BookmarkablePageLink<>("toHome", HomePage.class);
         add(toUserMakerLink);
+
+        var userInfoForm = new Form<>("userInfo");
+        add(userInfoForm);
+
+        var userNameField = new TextField<>("userName");
+        userInfoForm.add(userNameField);
+
+        var userPassField = new PasswordTextField("userPass");
+        userInfoForm.add(userPassField);
     }
 }
