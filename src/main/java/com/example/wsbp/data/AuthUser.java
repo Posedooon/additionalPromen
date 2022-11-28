df4a1cc20b24a374139c36a433a87321474ffc8a
Posedooon <b2200930@photon.chitose.ac.jp>
2022-11-29 01:20:58 +0900

    登録されているユーザの一覧を表示する02終了

--- src/main/java/com/example/wsbp/data/AuthUser.java
+++ src/main/java/com/example/wsbp/data/AuthUser.java
@@ -4,11 +4,12 @@ import java.io.Serializable;
 
 // AUTH_USER テーブルのデータを入れるクラス
 // Wicketの Model に使うかもしれないクラスは、 implements Serializable をつける
-public class AuthUser implements Serializable{
-    private final String userName; // auth_userテーブルのuser_name列のデータ
-    private final String userPass; // auth_userテーブルのuser_pass列のデータ
+public class AuthUser implements Serializable {
 
-    public AuthUser(String userName,String userPass){
+    private final String userName;  // auth_userテーブルのuser_name列のデータ
+    private final String userPass;  // auth_userテーブルのuser_pass列のデータ
+
+    public AuthUser(String userName, String userPass) {
         this.userName = userName;
         this.userPass = userPass;
     }
@@ -22,19 +23,20 @@ public class AuthUser implements Serializable{
     }
 
     @Override
-    public boolean equals(Object o){
-        if(this==o) return true;
-        if(o==null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
         AuthUser authUser = (AuthUser) o;
 
-        if(!userName.equals(authUser.userName)) return false;
+        if (!userName.equals(authUser.userName)) return false;
         return userPass.equals(authUser.userPass);
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         int result = userName.hashCode();
-        result = 31*result +userPass.hashCode();
+        result = 31 * result + userPass.hashCode();
         return result;
     }
 }
--- src/main/java/com/example/wsbp/repository/AuthUserRepository.java
+++ src/main/java/com/example/wsbp/repository/AuthUserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.security.SecureRandom;
 import java.util.List;
 
 @Repository
